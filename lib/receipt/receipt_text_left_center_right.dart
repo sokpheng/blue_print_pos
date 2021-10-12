778536883815299fce449a83fab210f0a165cc97
Sokpheng Cheang <sokphengcheang@gmail.com>
2021-10-12 16:30:31 +0700

    Update receipt_text_left_center_right.dart

--- lib/receipt/receipt_text_left_center_right.dart
+++ lib/receipt/receipt_text_left_center_right.dart
@@ -1,5 +1,7 @@
 import 'receipt_text_style.dart';
 import 'receipt_text_style_type.dart';
+import 'receipt_alignment.dart';
+import 'collection_style.dart';
 
 class ReceiptTextLeftCenterRight {
   ReceiptTextLeftCenterRight(
@@ -18,6 +20,7 @@ class ReceiptTextLeftCenterRight {
       type: ReceiptTextStyleType.normal,
       useSpan: true,
     ),
+    this.alignment = ReceiptAlignment.center,
   });
 
   final String leftText;
@@ -26,12 +29,22 @@ class ReceiptTextLeftCenterRight {
   final ReceiptTextStyle leftTextStyle;
   final ReceiptTextStyle centerTextStyle;
   final ReceiptTextStyle rightTextStyle;
+  final ReceiptAlignment alignment;
 
   String get html => '''
-    <p class="full-width inline-block">
+    <p class="$_alignmentStyleHTML full-width inline-block">
       <${leftTextStyle.textStyleHTML} class="left ${leftTextStyle.textSizeHtml}">$leftText</${leftTextStyle.textStyleHTML}>
-      <${centerTextStyle.textStyleHTML} class="center ${centerTextStyle.textSizeHtml}">$centerText</${centerTextStyle.textStyleHTML}>
+      <${centerTextStyle.textStyleHTML} class="$_alignmentStyleHTML ${centerTextStyle.textSizeHtml}">$centerText</${centerTextStyle.textStyleHTML}>
       <${rightTextStyle.textStyleHTML} class="right ${rightTextStyle.textSizeHtml}">$rightText</${rightTextStyle.textStyleHTML}>
     </p>
   ''';
+
+  String get _alignmentStyleHTML {
+    if (alignment == ReceiptAlignment.left) {
+      return CollectionStyle.textLeft;
+    } else if (alignment == ReceiptAlignment.right) {
+      return CollectionStyle.textRight;
+    }
+    return CollectionStyle.textCenter;
+  }
 }
